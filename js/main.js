a6156d5e033e8c8d75fb93d16bc48e0d933fc4dd
The Late Night Salon <helder@thelatenightsalon.co.uk>
2014-04-03 18:36:46 +0100

    made the columns and content responsive. started the sprite animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
+$(document).ready(function(){
 
+	/***
+	 *
+	 * Update some fonts on window resize
+	 *
+	 */
+	var maxSz = 650;
+	var font = 1;
+	var timeout = 1000;
+	var updateFontSize = function(){
+
+		if ( window.innerWidth >= maxSz ) {
+			return;
+		};
+
+		font = $('.dont-miss-out').width() / maxSz;
+		font = Math.round(font * 10) / 10;
+
+		$(".dont-miss-out").css("font-size", font + "em");
+
+	};
+
+	$(window).on('resize', _.throttle(updateFontSize, 400));
+
+	/***
+	 *
+	 *	Update the sprite image width
+	 *
+	 */
+	 $(window).on('resize', function(){
+
+		if ( window.innerWidth >= maxSz ) {
+			return;
+		};
+
+		originalW = 18490;
+		originalH = 383;
+		ratio = $('.helmet').width() / maxSz;
+
+		newSize = originalW/ratio + "px" + " " + originalH/ratio + "px";
+
+	 	$(".my-sprite").css("background-size", newSize);
+
+	 });
+
+});
