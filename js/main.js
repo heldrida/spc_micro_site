d51389ae7d05eb65dcbfd466d4fbccaf40230866
Helder Oliveira <heldrida@gmail.com>
2014-04-04 14:07:04 +0100

    created the countdown

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+	var mySettings = {
+		wp_base_path: window.location.hostname.indexOf('tlnscompetitions') > -1 ? '/emerge_space/wordpress' : 'http://emergespace.magnolia.thelatenightsalon.co.uk/wordpress'
+	};
+
 	/***
 	 *
 	 * Update some fonts on window resize
@@ -9,13 +13,9 @@ $(document).ready(function(){
 	var font = 1;
 	var timeout = 1000;
 	var updateFontSize = function(){
-
-		if ( window.innerWidth >= maxSz ) {
-			return;
-		};
-
+ 
 		font = $('.dont-miss-out').width() / maxSz;
-		font = Math.round(font * 10) / 10;
+		font = (font * 10) / 10;
 
 		$(".dont-miss-out").css("font-size", font + "em");
 
@@ -25,23 +25,68 @@ $(document).ready(function(){
 
 	/***
 	 *
-	 *	Update the sprite image width
+	 * Set clock
 	 *
 	 */
-	 $(window).on('resize', function(){
+	function initClock(myDate){
 
-		if ( window.innerWidth >= maxSz ) {
-			return;
-		};
+	    // set the date we're counting down to
+	    var target_date = new Date(myDate).getTime();
+	     
+	    // variables for time units
+	    var days, hours, minutes, seconds;
+	     
 
-		originalW = 18490;
-		originalH = 383;
-		ratio = $('.helmet').width() / maxSz;
+	    // update the tag with id "countdown" every 1 second
+	    setInterval(function () {
+	     
+	        // find the amount of "seconds" between now and target
+	        var current_date = new Date().getTime();
+	        var seconds_left = (target_date - current_date) / 1000;
+	     
+	        // do some time calculations
+	        days = parseInt(seconds_left / 86400);
+	        seconds_left = seconds_left % 86400;
+	         
+	        hours = parseInt(seconds_left / 3600);
+	        seconds_left = seconds_left % 3600;
+	         
+	        minutes = parseInt(seconds_left / 60);
+	        seconds = parseInt(seconds_left % 60);
+	         
+	        function getPrefix(x){
+	            
+	            if (x < 10) {
+	                x = "0" + x;
+	            };
 
-		newSize = originalW/ratio + "px" + " " + originalH/ratio + "px";
+	            return x;
+	        };
 
-	 	$(".my-sprite").css("background-size", newSize);
+	        $('#countdown span.days').text( getPrefix(days) );
+	        $('#countdown span.hours').text( getPrefix(hours) );
+	        $('#countdown span.minutes').text( getPrefix(minutes) );
+	        $('#countdown span.seconds').text( getPrefix(seconds) );
 
-	 });
+	    }, 1000);
+
+	};
+
+	$.post(
+		mySettings.wp_base_path + '/wp-admin/admin-ajax.php?action=space_competition',
+		{
+			option: "my_options",
+			get_option_name: "my_competition_options" 
+		},
+		function( data ){
+
+			data = JSON.parse(data);
+
+			var x = data.end_date.split("/");
+			var myDate = x[2] + "-" + x[1] + "-" + x[0];
+
+			initClock( myDate );
+
+	});
 
 });
